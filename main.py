abfe8533423437761c07f16d1fad9ecebd7aafa6
EstebanRoblero <estebanroblero10@gmail.com>
2025-02-12 11:38:19 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+print("----------Menu--------------")
+num= float(input("Ingrese numero entre 1-9:"))
+
+if (num==1):
+    print("el numero romano de 1 es I")
+elif(num ==2):
+    print("el numero romano de 2 es II")
+elif(num ==3):
+    print("el numero romano de 3 es III")      
+elif(num ==4):
+    print("el numero romano de 4 es IV ")    
+elif(num ==5):
+    print("el numero romano de 5 es V")
+elif(num ==6):
+    print("el numero romano de 6 es VI")
+elif(num ==7):
+    print("el numero romano de 7 es VII")
+elif(num ==8):
+    print("el numero romano de 8 es VIII")    
+elif(num ==9):
+    print("el numero romano de 9 es IX")
+    
+else:
+    print("--No esta dentro del rango solicitado--")
